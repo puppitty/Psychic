46630ad8851d18294a1593aadb189f0e7b20a8b3
puppitty <33668867+puppitty@users.noreply.github.com>
2018-02-16 11:25:47 -0500

    Working files. Now for some clean up

--- javascript/game.js
+++ javascript/game.js
@@ -7,76 +7,73 @@ var computerGuessHTML = document.getElementById('computer-guess');
 
 var resetButton = document.getElementById('reset-button');
 
-
-
-// Creating variables to hold the number of wins, losses, and ties. They start at 0.
+// Creating variables to hold the number of wins, losses, and ties. They start at 0, except number of games.
 var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
 var lettersGuessed = [];
 
-var game = {
-reWriteStats: function () {
-  this.wins = 0;
-  this.losses = 0;
-  this.guessesLeft = 10;
-  this.lettersGuessed = [];
-  numberWinsHTML.textContent = 0;
-  numberWinsHTML.textContent = 0;
-  numberLossesHTML.textContent = 0;
-  guessesLeftHTML.textContent = 10;
-  lettersGuessedHTML.textContent = [];
-  computerGuessHTML.textContent = "";
-
-}}
-
-// Creates an array that lists out all of the options (Guess the letter).
+// Creates an array that lists out all of the letters (Guess the letter).
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
   "s", "t", "u", "v", "w", "x", "y", "z"];
 
+var game = {
+// Function to reset the game back to beginning
+  reWriteStats: function () {
+    wins = 0;
+    losses = 0;
+    guessesLeft = 10;
+    lettersGuessed = [];
+    numberWinsHTML.textContent = 0;
+    numberWinsHTML.textContent = 0;
+    numberLossesHTML.textContent = 0;
+    guessesLeftHTML.textContent = 10;
+    lettersGuessedHTML.textContent = [];
+    computerGuessHTML.textContent = "";
+
+  }
+}
 // This function is run whenever the user presses a key.
 document.onkeyup = function (event) {
 
   // Determines which key was pressed.
   var userGuess = event.key;
   lettersGuessed.push(userGuess);
-  lettersGuessedHTML.textContent = this.lettersGuessed;
 
-  // console.log(userGuess)
-
-  // Randomly chooses a choice from the options array. This is the Computer's guess.
+  // Randomly chooses from the options array. This is the Computer's guess.
   var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
+  // checks UserGuess versus Computer guess. If equal, then add one to wins
   if (userGuess === computerGuess) {
     wins++;
-    numberWinsHTML.textContent = this.wins;
-
+    
+// If doesn't match, then adds one to losses
   } else {
     losses++;
-    numberLossesHTML.textContent = this.losses;
-
   }
+
+  // Subtracts 1 from guesses left
   guessesLeft--;
-  guessesLeftHTML.textContent = this.guessesLeft;
+
+  // Adds the guessed letter to the array
   lettersGuessed.join(", ");
 
+// Checks to see if there are any guesses left. If not, then resets the values
   if (guessesLeft === 0) {
     wins = 0;
     losses = 0;
     guessesLeft = 10;
     lettersGuessed = [];
   }
-  
-  // Creating a variable to hold our new HTML. Our HTML now keeps track of the user and computer guesses, and wins/losses/ties.
-  var html = "<p>Guess what letter I'm thinking of: </p>" +
-    "<p>Your choice: " + lettersGuessed.join(', ') + "</p>" +
-    "<p>The computer's choice: " + computerGuess + "</p>" +
-    "<p>Wins: " + wins + "</p>" +
-    "<p>Losses: " + losses + "</p>" +
-    "<p>Guesses Left: " + guessesLeft + "</p>";
 
-  // Set the inner HTML contents of the #game div to our html string
-  document.querySelector("#game").innerHTML = html;
+// Writes the values to the HTML for display
+  var html = 
+  numberWinsHTML.textContent = wins;
+  numberLossesHTML.textContent = losses;
+  guessesLeftHTML.textContent = guessesLeft;
+  computerGuessHTML = computerGuess;
+  lettersGuessedHTML.textContent = lettersGuessed.join(', ');
 };
 
+// Listens forhte reset button and changes all the values back to the beginnning values.
 resetButton.addEventListener('click', game.reWriteStats);
